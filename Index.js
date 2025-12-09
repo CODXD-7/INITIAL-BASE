da1acc3a2689f55ff031c17db999f56a336cdb0a
CODXD-7 <amb1v1@outlook.com>
2025-12-09 07:41:51 -0800

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,66 @@
+IRAC-ANALYSIS{
+	JUDGE{
+		state: "CA",
+		caseNumber: "#",
+		courtHouse: "#",
+		department: "#"
+		ruling: "In Effect",
+		order: "Legal/Experienced Counsel to step in and oversee Unions/Enterprises' Livelihood"
+        }
+	CEO-PRESIDENT{
+		gvmntName: "#",
+		ethnicity: "#",
+		age: 00,
+		gender: "#"
+		majorInField: "##",
+		contract: "Temporary",
+		}
+	SR-VICE-PRESIDENT {
+		gvmntName: "#",
+		ethnicity: "#",
+		age: 00,
+		gender: "#"
+		majorInField: "##",
+		contract: "Temporary"
+	}
+	VICE-PRESIDENT {
+		gvmntName: "#",
+		ethnicity: "#",
+		age: 00,
+		gender: "#"
+		majorInField: "##",
+		contract: "Temporary",
+	}
+	BANKRUPTCY-ATTORNEY {
+		gvmntName: "#",
+		ethnicity: "#",
+		age: 00,
+		gender: "#"
+		majorInField: "##",
+		contract: "Temporary",
+	}
+	PHILONTROPHIST-X {
+		gvmntName: "#",
+		ethnicity: "#",
+        age: 00,
+		gender: "#"
+		majorInField: "REAL-ESTATE, HEMISPHERE RESEARCHER",
+		contract: "TAX-EXEMPT CHARITY DISCLOSURE"
+	}
+	PHILONTOPHIST-Y {
+		gvmntName: "Ta-LEAD",
+		ethnicity: "#",
+		age: 000,
+		gender: "#"
+		majorInField: "PHD NUERO SCIENCE & BIO-ENGINEERING",
+		contract: "❄️❄️Silicon Valley Recognized"❄️❄️
+	}
+    PHILONTROPHIST-Z {
+    	gvmntName: "#",
+		ethnicity: "#",
+		age: 00,
+		gender: "#"
+		majorInField: "FIDICUARY",
+		contract: "Vanguards' SWEEPSTAKES Security Funding"
+	}
+}
